68291df0241e36624c7defce6e6b1895193ad75e
Raymond Osy <raymondosy1@gmail.com>
2018-07-24 07:21:08 +0100

    Implement LFA feedback on Index, Signup and login page

---
+++ dist/server/dummyDb/dummyDb.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var entries = [{
+  entryId: 1,
+  entryBody: 'First Entry Body',
+  dateTime: 'today',
+  userId: 3
+}, {
+  entryId: 2,
+  entryContent: 'Second Entry Body',
+  dateTime: 'yesterday',
+  userId: 1
+}, {
+  entryId: 1,
+  entryContent: 'Third Entry Body',
+  dateTime: '3 days ago',
+  userId: 2
+}];
+
+exports.default = entries;
