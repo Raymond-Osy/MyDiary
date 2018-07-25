4d32b849b085111223a73feb6cfe4269e2ebbe0f
Raymond Osy <raymondosy1@gmail.com>
2018-07-25 23:11:18 +0100

    Install mocha, chai and  chai-http

---
+++ server/test/test.js
@@ -0,0 +1,19 @@
+import chaiHttp from 'chai-http';
+import chai, { expect } from 'chai';
+import app from '../app';
+
+/* eslint-disable no-unused-vars */
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Get a non existing url/page', () => {
+  it('Should return 404 for unknown routes', (done) => {
+    chai.request(app)
+      .get('/invalid/route')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      });
+  });
+});
