f5a4c7b33036b7c5a5626b9050bda767c547a59e
Raymond Osy <raymondosy1@gmail.com>
2018-07-27 00:21:21 +0100

    Implement feedback for the buttons, toggle, responsiveness

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,10 +1,18 @@
 // This is for toggling the openNav and closeNav button
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+//     document.getElementById("main").style.marginLeft = "250px";
+// }
+
+// function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//     document.getElementById("main").style.marginLeft= "0";
+// }
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
 }
